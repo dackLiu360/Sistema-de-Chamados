6d01ccc958678d0d3bc9f6758244ef470492ac19
31428258 <31428258@089790-P33S09.macklabs.br>
2016-10-21 19:52:44 -0200

    Estrutura do netBeans

---
+++ SistemaDeChamados/test/Testes/TesteCliente.java
@@ -0,0 +1,18 @@
+package Testes;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author 31428258
+ */
+public class TesteCliente {        
+    
+    @Test
+    public void testConcatenate() {        
+        int teste = 10;               
+
+        assertEquals(10, teste);
+    }       
+}
