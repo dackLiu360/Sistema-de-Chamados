2b0ff57c4ad9769fda2ef16ecab34a99c6787af4
lucas.live.sds@hotmail.com <lucas.live.sds@hotmail.com>
2016-11-01 17:31:37 -0200

    Feito algumas alterações

---
+++ SistemaDeChamados/test/TesteChamados.java
@@ -0,0 +1,24 @@
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Lucas
+ */
+public class TesteChamados{        
+    
+    @Test
+    public void testConcatenate() {        
+        int teste = 10;               
+
+        assertEquals(10, teste);
+    }       
+}
+
